e2f81f211622e4cbfaa7de1af67b43c5e2e4dd6e
Sander <89039740+sandrewTx08@users.noreply.github.com>
2021-11-09 16:02:01 -0300

    url pattern
    - speed up pages request with sessions
    - apply pattern on catalog requests
    - alter relationship between classes
    - append tuple

--- gearbest_scraper/scraper.py
+++ gearbest_scraper/scraper.py
@@ -1,37 +1,30 @@
 from os import chdir, path as sys_path
 from tqdm import trange
-from requests import get
 from lxml.etree import HTML
 from json import load
 from sqlite3 import connect
+import requests
 
 
 class Attributes(object):
     """ Page objects """
     
-    def page_set(self, url):
-        self.page = self.request(url)
-
     @property
-    def page_count_all(self):
-
-        if self.page != None:
-            
+    def page_count_all(self):           
+        if self.page_root != None:
             try:
-                return int(self.page.xpath(
+                return int(self.page_root.xpath(
                     '//footer/div[2]/div/a[last()-1]')[0].xpath('string()'))
 
             except IndexError:
                 return 0
-
-        elif self.page == None:
+        else:
             return 0
-
+    
     @property
     def currency(self):
-        
         try:
-            return self.page.xpath(
+            return self.page_root.xpath(
                 '//li[@id="js-btnShowShipto"]/span/span[2]')[0].xpath('string()')
         
         except IndexError:
@@ -39,141 +32,119 @@ class Attributes(object):
 
     @property
     def search_category(self):
-
         if self.page_count_all != 0:
             try:
-                return self.page.xpath(
+                return self.page_root.xpath(
                     '//div[@class="cateMain_asideSeachTitle"]/a[@title]'
                     )[0].get('title').replace("'", '').replace('"', '')
 
             except IndexError:
                 try:
-                    return self.page.xpath(
+                    return self.page_root.xpath(
                         '//div[@class="searchTitle_wrap"]/h1'
                         )[0].xpath('string()').replace("'", '').replace('"', '')
 
                 except IndexError:
                     return None 
 
-    def catalog(self, page_with_catalog):
+    def catalog(self, page):
         """ Information ads  generator """
+        page = self.request(page)
+        
+        if page != None:
+            # Catalog list parent element
+            catalog_list_box = page.xpath(
+                '//ul[@class="clearfix js_seachResultList"]')[0]
 
-        page_with_catalog = self.request(page_with_catalog)
+            if catalog_list_box.find('li[@data-goods-id]') != None:
+                # Catalog list children elements
+                catalog_list = catalog_list_box.xpath(
+                    '//li[@data-goods-id]') 
 
-        # Catalog list parent element
-        catalog_list_box = page_with_catalog.xpath(
-            '//ul[@class="clearfix js_seachResultList"]')[0]
+                # Avaliable catalog on page
+                catalog_count = range(len( 
+                    catalog_list_box.xpath(
+                        '//li[@data-goods-id]')))
 
-        if catalog_list_box.find('li[@data-goods-id]') != None:
+                price = [ float(catalog_list[i].get(
+                            'data-shopprice'))
+                        for i in catalog_count ]
+                
+                id = [ catalog_list[i].get(
+                            'data-goods-id')
+                        for i in catalog_count ]
+                        
+                image = [ catalog_list[i].xpath(
+                            '//a[@data-img]/img[@data-lazy]'
+                                )[i].get('data-lazy')
+                                    .replace("'", '')
+                                    .replace('"', '')
+                        for i in catalog_count ]
+            
+                title = [ catalog_list[i].xpath(
+                            '//div[@class="gbGoodsItem_outBox"]/p/a'
+                                )[i].get('title')
+                                    .replace("'", '')
+                                    .replace('"', '')
+                        for i in catalog_count ]
+                
+                link = [ catalog_list[i].xpath(
+                        '//div[@class="gbGoodsItem_outBox"]/p/a'
+                            )[i].get('href')
+                                .replace("'", '')
+                                .replace('"', '')
+                        for i in catalog_count ]
+                
+                discount = [ int(catalog_list[i].find('span/strong').xpath('string()'))
+                            if catalog_list[i].find('span/strong') != None
+                            else None
+                        for i in catalog_count ]
+                
+                review_rate = [ float(catalog_list[i].find(
+                            'div/div[2]/span/span[2]').xpath('string()'))
+                        if catalog_list[i].find(
+                            'div/div[2]/span/span[2]') != None
+                        else None
+                        for i in catalog_count ]
+                
+                review_count = [ int(catalog_list[i].xpath(
+                            '//li[@data-goods-id]')[i].find(
+                                'div/div[2]/span/span[3]').xpath('string()')
+                            .replace('(', '')
+                            .replace(')', ''))
+                        if catalog_list[i].find(
+                            'div/div[2]/span/span[3]') != None
+                        else None
+                        for i in catalog_count ]
+                
+                price_tag = [
+                        catalog_list[i].find(
+                            'div/div[1]/a/span').get('title')
+                            .replace('Warehouse', '')
+                            .replace("'", '')
+                            .replace('"', '')
+                        if catalog_list[i].find('div/div[1]/a/span') != None
+                        else catalog_list[i].find('div/div[1]/span').xpath('string()')
+                        if catalog_list[i].find('div/div[1]/span') != None
+                        else None
+                        for i in catalog_count ]
 
-            # Catalog list children elements
-            catalog_list = catalog_list_box.xpath(
-                '//li[@data-goods-id]') 
-
-            # Avaliable catalog on page
-            catalog_count = range(len( 
-                catalog_list_box.xpath(
-                    '//li[@data-goods-id]')))
-
-            price = [
-                    float(catalog_list[i].get(
-                        'data-shopprice'))
-                    for i in catalog_count
-            ]
-            
-            id = [
-                    catalog_list[i].get(
-                        'data-goods-id')
-                    for i in catalog_count
-            ]
-            
-            image = [
-                    catalog_list[i].xpath(
-                        '//a[@data-img]/img[@data-lazy]')[i].get('data-lazy')
-                        .replace("'", '')
-                        .replace('"', '')
-                    for i in catalog_count
-            ]
-            
-            title = [
-                    catalog_list[i].xpath(
-                        '//div[@class="gbGoodsItem_outBox"]/p/a')[i].get('title')
-                        .replace("'", '')
-                        .replace('"', '')
-                    for i in catalog_count
-            ]
-            
-            link = [
-                    catalog_list[i].xpath(
-                        '//div[@class="gbGoodsItem_outBox"]/p/a')[i].get('href')
-                        .replace("'", '')
-                        .replace('"', '')
-                    for i in catalog_count
-            ]
-            
-            discount = [
-                    int(catalog_list[i].find(
-                        'span/strong').xpath('string()'))
-                    if catalog_list[i].find(
-                        'span/strong') != None
-                    else None
-                    for i in catalog_count
-            ]
-            
-            review_rate = [
-                    float(catalog_list[i].find(
-                        'div/div[2]/span/span[2]').xpath('string()'))
-                    if catalog_list[i].find(
-                        'div/div[2]/span/span[2]') != None
-                    else None
-                    for i in catalog_count
-            ]
-            
-            review_count = [
-                    int(catalog_list[i].xpath(
-                        '//li[@data-goods-id]')[i].find(
-                            'div/div[2]/span/span[3]').xpath('string()')
-                        .replace('(', '')
-                        .replace(')', ''))
-                    if catalog_list[i].find(
-                        'div/div[2]/span/span[3]') != None
-                    else None
-                    for i in catalog_count
-            ]
-            
-            price_tag = [
-                    catalog_list[i].find(
-                        'div/div[1]/a/span').get('title')
-                        .replace('Warehouse', '')
-                        .replace("'", '')
-                        .replace('"', '')
-                    if catalog_list[i].find('div/div[1]/a/span') != None
-                    else catalog_list[i].find('div/div[1]/span').xpath('string()')
-                    if catalog_list[i].find('div/div[1]/span') != None
-                    else None
-                    for i in catalog_count
-            ]
-
-            # Provide last id from 'table_search'
-            last_id = self.cursor.execute(
-                f""" SELECT MAX ({self.stble['id']})
-                    FROM {self.stble['name']}; """).fetchone()[0]
-            
-            for i in catalog_count:       
-                yield (
-                    id[i], 
-                    title[i],
-                    link[i],
-                    image[i],
-                    price[i],
-                    discount[i],
-                    price_tag[i],
-                    review_rate[i],
-                    review_count[i],
-                    last_id)
-            
+                for i in catalog_count:       
+                    yield [
+                        id[i], 
+                        title[i],
+                        link[i],
+                        image[i],
+                        price[i],
+                        discount[i],
+                        price_tag[i],
+                        review_rate[i],
+                        review_count[i]
+                    ]
+                
     def link_generator(self, page):
-
+        page = self.request(page)
+        
         for parent_link in page.xpath(
             '//li[@class="headCate_item"]/a[@class="headCate_itemName"]'):
 
@@ -185,6 +156,7 @@ class Attributes(object):
             yield child_link.get('href')
     
     def popular_searches(self, page):
+        page = self.request(page)
         
         for pop_item in page.xpath(
             '//li[@class="footerHotkey_item"]/a'):
@@ -192,11 +164,10 @@ class Attributes(object):
             yield (pop_item.get('href'), pop_item.xpath('string()'))
 
 
-class Database(object):
+class Database(Attributes):
     """ Database handler """
 
     def __init__(self, path):
-        
         # Load configuration file
         try:
             self.conf = load(open(path))
@@ -253,7 +224,6 @@ class Database(object):
     
     @property
     def currency_id(self):
-    
         currency_id = self.cursor.execute(  
             f""" SELECT MAX({self.crtble['id']}), {self.crtble['abbreviation']} 
                     FROM {self.crtble['name']} 
@@ -272,19 +242,18 @@ class Database(object):
         elif currency_id[1] == self.currency:
             return currency_id[0]
     
-    def table_search(self, name_keyword): 
-        
-        if name_keyword != None:
-            name_keyword_to_database = name_keyword.replace(
-                "'", '').replace('"', '')
-
-        elif name_keyword == None:
-            name_keyword_to_database = None
-        
+    def table_search(self, keyword):        
         if self.page_count_all != 0:
             
+            keyword_to_database = (
+                keyword
+                    .replace("'", '')
+                    .replace('"', '')
+                if keyword != None
+                else None)
+            
             values = (
-                name_keyword_to_database,
+                keyword_to_database,
                 self.search_category,
                 self.page_count_all,
                 self.currency_id)
@@ -302,108 +271,100 @@ class Database(object):
             self.database.commit()
 
     def table_catalog(self, page_with_catalog):
-        
-        for catalog in self.catalog(page_with_catalog):
+        # Provide last id from 'table_search'
+        last_id = [self.cursor.execute(
+               f""" SELECT MAX ({self.stble['id']})
+                   FROM {self.stble['name']}; """).fetchone()[0]]
+
+        for catalog in self.catalog(page_with_catalog):           
+            # Concat catalog with last id as tuple
+            catalog_concat = tuple(catalog) + tuple(last_id) 
+            
             self.cursor.execute(
                 f""" INSERT INTO {self.ctble['name']}  
-                    VALUES (?,?,?,?,?,?,?,?,?,?); """, catalog)
+                    VALUES (?,?,?,?,?,?,?,?,?,?); """, catalog_concat)
 
 
-class Method(Database, Attributes):
+class Method(Database):
     """ Request page ads """
+    session = requests.Session()
 
     def request(self, url):
         """ Return page object """
-    
-        page = get(url, 
-            timeout=120, 
+        page = self.session.get(url, 
+            timeout=120,
             params=self.conf['http_header'])
-
+        
         if page.status_code == 200:
             return HTML(page.text)
+    
+    def scrape_pattern(self, 
+                            page_with_ads, 
+                            table_search_keyword_name, 
+                            range_display_name, 
+                            page_root_url):
+        """ Scrape pages based on url pattern """
+        self.page_root = self.request(page_root_url)
+        self.table_search(table_search_keyword_name)
+        
+        for page_number in trange(self.page_count_all, 
+                                                    colour='WHITE', 
+                                                    desc=range_display_name):
+            self.table_catalog(
+                '{}{}.html?page_size=120?odr=relevance'
+                .format(page_with_ads, page_number + 1)
+            )       
 
     def scrape_by_search_bar(self):
-
         for search in self.conf['search_list']:
-            
-            search_to_url = search.replace(" ", "-")
-
             # Page used to refer primary values
-            self.page_set(
-                'https://www.gearbest.com/sale/{}?page_size=120?odr=relevance'
-                .format(search_to_url))
-
-            self.table_search(search)
-
-            for page_number in trange(self.page_count_all, colour='WHITE', desc=search):
-                                     
-                self.table_catalog(
-                    'https://www.gearbest.com/sale/{}/{}.html?page_size=120?odr=relevance'
-                    .format(search_to_url, page_number + 1)
-                )       
-        
+            url = 'https://www.gearbest.com/sale/{}/'.format(search)
+            
+            self.scrape_pattern(
+                page_with_ads = url,
+                page_root_url = url,
+                table_search_keyword_name = search,
+                range_display_name = search)
         self.database.close()
 
     def scrape_by_link_generator(self):
-
         # Page used to refer primary values
-        page_menu = self.request('https://www.gearbest.com/')
-
-        for url in self.link_generator(page_menu):
-
-            self.page_set(
-                '{}?page_size=120?odr=relevance'
-                .format(url)
-            )
-            
-            self.table_search(None)
-
-            for page_number in trange(self.page_count_all, colour='WHITE', desc=url):
-
-                self.table_catalog(
-                    '{}{}.html?page_size=120?odr=relevance'
-                    .format(url, page_number + 1)
-                )
-
+        menu_url = 'https://www.gearbest.com/'
+        
+        for url in self.link_generator(menu_url):
+            self.scrape_pattern(
+                page_with_ads = url,
+                page_root_url = url,
+                table_search_keyword_name = None,
+                range_display_name = url)      
         self.database.close()
 
     def scrape_by_popular_searches(self):
+        # Page used to refer primary values
+        menu_url = 'https://www.gearbest.com/'
         
-        page_menu = self.request('https://www.gearbest.com/')
-                
-        for pop_item in self.popular_searches(page_menu):
-            
-            # Page used to refer primary values
-            self.page_set(
-                '{}?page_size=120?odr=relevance'
-                .format(pop_item[0])
-            )
-            
-            self.table_search(
-                pop_item[1],
-            )
-
-            for page_number in trange(self.page_count_all, colour='WHITE', desc=pop_item[1]):
-
-                self.table_catalog(
-                    '{}{}.html?page_size=120?odr=relevance'
-                    .format(pop_item[0], page_number + 1)
-                )   
-
+        for pop_item in self.popular_searches(menu_url):
+            self.scrape_pattern(
+                page_with_ads = pop_item[0],
+                page_root_url = pop_item[0],
+                table_search_keyword_name = pop_item[1],
+                range_display_name = pop_item[1])
         self.database.close()
 
 
 def run(path, mode):   
     """ Execute the Gearbest_Scraper """
-    
     method = Method(path)
     
     if mode == 'search' or mode.startswith('s'):
+        # >> python --mode search
         method.scrape_by_search_bar()       
 
     elif mode == 'link' or mode.startswith('l'):
+        # >> python --mode link
         method.scrape_by_link_generator()
     
     elif mode == 'popular' or mode.startswith('p'):
+        # >> python --mode popular
         method.scrape_by_popular_searches()
 
